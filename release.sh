be6059b7f6c6c5cda9481b57bdf02d7a5aafa09c
peng5545 <128353632+peng5545@users.noreply.github.com>
2024-08-18 21:32:24 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -5,4 +5,4 @@ cp app/build/outputs/apk/release/app-armeabi-v7a-release.apk xray-plugin-armeabi
 cp app/build/outputs/apk/release/app-arm64-v8a-release.apk xray-plugin-arm64-v8a-${release}.apk
 cp app/build/outputs/apk/release/app-x86-release.apk xray-plugin-x86-${release}.apk
 cp app/build/outputs/apk/release/app-x86_64-release.apk xray-plugin-x86_64-${release}.apk
-cp app/build/outputs/apk/release/app-universal-release.apk xray-plugin--universal-${release}.apk
+cp app/build/outputs/apk/release/app-universal-release.apk xray-plugin-universal-${release}.apk
